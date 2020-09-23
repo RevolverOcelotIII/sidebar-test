5f958baa929931774e7bf03be1fe899bd129a174
Revolver Ocelot(Revolver Ocelot) <danpgaldino@gmail.com>
2020-09-23 10:12:28 -0300

    Revert "yeah"
    This reverts commit 46885cf139b774568fabc7253a20a8e070cf870b.

--- sidebar-teste/src/App.js
+++ sidebar-teste/src/App.js
@@ -5,44 +5,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Reports from './pages/Reports';
-
-
-function App(props) {
-
-  function onShown() {
-    console.log('diagram shown');
-  }
-
-  function onLoading() {
-    console.log('diagram loading');
-  }
-
-  function onError(err) {
-    console.log('failed to show diagram');
-  }
-
-  return (
-    <Products
-      url="\newDiagram.bpmn"
-      onShown={ onShown }
-      onLoading={ onLoading }
-      onError={ onError }
-    />
-  );
-}
-/*App() {
+function App() {
   return (
     <>
     <Router>
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home}></Route>
-        <Route path='/products'><Products url='./newDiagram.bpmn'/></Route>
+        <Route path='/products' component={Products}></Route>
         <Route path='/reports' component={Reports}></Route>
       </Switch>
     </Router>
     </>
   );
-}*/
+}
 
 export default App;
